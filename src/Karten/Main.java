afe76b48e7d1e706eee4e8bd055ef41916888d20
Leldom <Kannix1402@gmail.com>
2025-01-14 22:04:19 +0100

    nothing

--- src/Karten/Main.java
+++ src/Karten/Main.java
@@ -14,12 +14,13 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         char c = scan.next().charAt(0);
         
-            switch(c) { 
-                case 'e'-> KartenAktion.erstelleKarte();
-                case 'a'-> {System.out.println("Noch nicht implementiert!"); checkInput();}
-                case 'q'-> {System.out.print("Erstellung abgebrochen "); System.exit(0);} 
-                default -> {
-                   System.out.println("Ungueltiger Wert. Bitte neu eingeben: "); checkInput();}
+        switch(c) { 
+            case 'e'-> KartenAktion.erstelleKarte();
+            case 'a'-> {System.out.println("Noch nicht implementiert!"); checkInput();}
+            case 'q'-> {System.out.print("Erstellung abgebrochen "); System.exit(0);} 
+            default -> {
+               System.out.println("Ungueltiger Wert. Bitte neu eingeben: "); checkInput();
             }
+        }
     }
 }
